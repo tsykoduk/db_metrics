145b29843e1d61e0cb33dd4a6e73424686dc2617
Greg Nokes <tsykoduk@gmail.com>
2012-09-14 16:40:22 -0700

    remove vmstat. use iostat for all of the things

--- db_metrics.rb
+++ db_metrics.rb
@@ -7,15 +7,15 @@ load_stat = "Load Average: " + uptime.split(':')[3].chomp.lstrip
 
 # => "1.40 1.23 1.43"
 
-vmstat = `vmstat`
+#vmstat = `vmstat`
 
 # output should look like this
 # => "procs -----------memory---------- ---swap-- -----io---- -system-- ----cpu----\n 
 #    r  b   swpd   free   buff  cache   si   so    bi    bo   in   cs us sy id wa\n 
 #    2  0 232684 479652 21943084 17665440  111   77   123   826    6    0 27  7 63  3\n"
 
-headers_vmstat = vmstat.split(/\r?\n/)[1]
-metrics_vmstat = vmstat.split(/\r?\n/)[2]
+#headers_vmstat = vmstat.split(/\r?\n/)[1]
+#metrics_vmstat = vmstat.split(/\r?\n/)[2]
 
 iostat = `iostat -d`
 
@@ -27,8 +27,8 @@ metrics_iostat = iostat.split(/\r?\n/)[2]
 #inject into log stream somehow here.
 
 puts load_stat
-puts headers_vmstat
-puts metrics_vmstat
+#puts headers_vmstat
+#puts metrics_vmstat
 puts headers_iostat
 puts details_iostat
 puts metrics_iostat
