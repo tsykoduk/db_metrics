7f9aa9f9dae2d3c20c4238fda0af214006147910
Greg Nokes <tsykoduk@gmail.com>
2012-09-15 16:51:06 -0700

    moving to iostat, one report per line, etc

--- db_metrics.rb
+++ db_metrics.rb
@@ -1,34 +1,35 @@
-uptime = `uptime`
+# db_metrics.rb will parse iostat and pull the load, the CPU stats and the disk load averaged
+# over one second. It's intended to be used to inject these values into a log stream
 
-load_stat = "Load Average: " + uptime.split(':')[3].chomp.lstrip
+# It depends on a sane io_stat output like this
 
-# => "1.40 1.23 1.43"
+#       disk0           disk2           disk5       cpu     load average
+# KB/t tps  MB/s     KB/t tps  MB/s     KB/t tps  MB/s  us sy id   1m   5m   15m
+#16.58  22  0.36   212.46   0  0.00    10.20   0  0.00  10  6 84  0.87 0.95 1.09
+#16.00   1  0.02     0.00   0  0.00     0.00   0  0.00   8  6 86  1.12 1.00 1.10
 
-#vmstat = `vmstat`
-#headers_vmstat = vmstat.split(/\r?\n/)[1]
-#metrics_vmstat = vmstat.split(/\r?\n/)[2]
 
+report = Array.new
+x = 0
 iostat = `iostat -c2`
 
 headers_iostat = iostat.split(/\r?\n/)[0]
 details_iostat = iostat.split(/\r?\n/)[1]
 metrics_iostat = iostat.split(/\r?\n/)[3]
 
-#output
+array_of_headers = headers_iostat.split
+array_of_metrics = metrics_iostat.split
+array_of_details = details_iostat.split
 
-#~/Code/db_metrics ☯  iostat -c 2 
-#          disk0           disk2           disk5       cpu     load average
-#    KB/t tps  MB/s     KB/t tps  MB/s     KB/t tps  MB/s  us sy id   1m   5m   15m
-#   16.77  22  0.36   221.35   0  0.00    11.33   0  0.00  10  6 83  5.92 5.58 5.83
-#   28.00   5  0.14     0.00   0  0.00     0.00   0  0.00  26 12 62  5.92 5.58 5.83
-
-#TODO put each disk on it's own line
-
-#inject into log stream somehow here.
-
-puts load_stat
-#puts headers_vmstat
-#puts metrics_vmstat
-puts headers_iostat
-puts details_iostat
-puts metrics_iostat
+array_of_headers.each { |a| 
+  unless a == "average"
+    report << a + ", " + 
+              array_of_details[x] + ": " + array_of_metrics[x] + ", " + 
+              array_of_details[x+1] + ": " + array_of_metrics[x+1] + ", " + 
+              array_of_details[x+2] + ": " + array_of_metrics[x+2]
+  
+    x = x + 3
+  end
+  }
+  
+report.reverse.each {|r| puts r }
